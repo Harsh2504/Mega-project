879061569340a156488d48a6cd4abb09334039fe
Harshvardhan Patil <104843006+Harsh2504@users.noreply.github.com>
2023-12-08 00:28:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2317,12 +2317,12 @@ def delsubd():
 
 
 
-#if __name__ == "__main__":
- #   app.run(debug=True, port=8000)
-import os
+if __name__ == "__main__":
+   app.run(debug=True, port=8000)
+#import os
 
-port = int(os.environ.get('PORT', 8000)) # default port is 5000
-app.run(host='0.0.0.0', port=port)
+#port = int(os.environ.get('PORT', 8000)) # default port is 5000
+#app.run(host='0.0.0.0', port=port)
 
 
 
